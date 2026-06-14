ba4aa21d434f0ec0b5f457958593cebd49c87e59
tejas101k <tejassingh649@rediffmail.com>
2026-06-14 20:11:05 +0000

    OmniJaws: Adapt for our use
    # Conflicts:
    #	app/src/main/java/org/omnirom/omnijaws/ui/components/HourlyForecastCard.kt

--- app/src/main/java/org/omnirom/omnijaws/icon/IconProvider.java
+++ app/src/main/java/org/omnirom/omnijaws/icon/IconProvider.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 
-import com.android.internal.util.crdroid.OmniJawsClient;
+import com.android.internal.util.infinity.OmniJawsClient;
 
 
 public class IconProvider {
--- app/src/main/java/org/omnirom/omnijaws/widget/WeatherAppWidgetProvider.kt
+++ app/src/main/java/org/omnirom/omnijaws/widget/WeatherAppWidgetProvider.kt
@@ -26,7 +26,7 @@ import android.widget.RemoteViews
 
 import androidx.preference.PreferenceManager
 
-import com.android.internal.util.crdroid.OmniJawsClient
+import com.android.internal.util.infinity.OmniJawsClient
 
 import org.omnirom.omnijaws.Config
 import org.omnirom.omnijaws.R
